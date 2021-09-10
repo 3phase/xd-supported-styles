6dfd97aa12e1ae15793bad7e5264afc72d4b1060
3phase <pbozhinow@gmail.com>
2021-09-10 14:48:14 +0300

    Adds parser that displays unsupported css properties

--- index.js
+++ index.js
@@ -1,33 +1,102 @@
-import { $, jQuery } from 'jquery';
-
-window.$ = $;
-window.jQuery = jQuery;
-
-export var Supported = function (target, cssCommands) {
-    var self = this;
-    self.target = target;
-    self.cssCommands = cssCommands;
-    self.placeholderId = "cssborder-";
-    self.domParser = new DOMParser();
-    self.targetDom = self.getTargetDom();
-    self.css;
+var Supported = function (target, cssCommands) {
+    this.target = target;
+    this.cssCommands = cssCommands;
+    this.placeholderId = "cssborder-";
+    this.domParser = new DOMParser();
+    this.targetDom = "";
+    this.css = "";
+    this.invalidRules = [];
+    this.loadPrerequesities().then(() => {
+        const cssPairs = this.parseCss();
+        this.validateCss(cssPairs);
+        let invalidRulesHTML = '';
+        this.invalidRules.forEach(rule => {
+            invalidRulesHTML += `<li>${rule}</li>`;
+        });
+        document.getElementById("content-projection").innerHTML = invalidRulesHTML;
+    });
 }
 
-Supported.prototype.init = function () {
+Supported.prototype.loadPrerequesities = function () {
     var self = this;
-    self.get();
+    return new Promise(async (res) => {
+        self.targetDom = await self.getTargetDom();
+        self.css = await self.fetchCss();
+        self.css = self.css.replace(/[\r\n]+/g, "");
+        res();
+    });
 }
 
 Supported.prototype.getTargetDom = function () {
     var self = this;
-    const xhr = new XMLHttpRequest();
-    xhr.open("get", self.target);
-    xhr.send();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState) {
-            console.log(xhr.responseText);
+    return new Promise((res) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("get", self.target);
+        xhr.send();
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                res(self.domParser.parseFromString(xhr.responseText, "text/html"));
+            }
         }
-    }
+    });
 };
 
-// const runner = new Supported("http://localhost/adobe-plugin-ref.html", "http://localhost/style.css");
+Supported.prototype.fetchCss = function () {
+    var self = this;
+    return new Promise((res) => {
+        fetch(self.cssCommands)
+            .then(data => res(data.text()));
+    });
+}
+
+Supported.prototype.parseCss = function () {
+    var self = this;
+    // all rules
+    let cssRules = self.css.split(/[{}]/).filter((x, i) => i % 2 !== 0);
+    // each rule pair
+    const cssRulesPairs = [];
+    cssRules = cssRules.map(x => x.trim());
+    cssRules.forEach(rule => {
+        let x = rule.split(';');
+        x = x.filter(function (str) {
+            return /\S/.test(str);
+        });
+        x = x.map(y => y.trim());
+        cssRulesPairs.push(...x);
+    });
+    return cssRulesPairs;
+}
+
+Supported.prototype.validateCss = function (pairs) {
+    var self = this;
+    for (const pair of pairs) {
+        let rule = pair.split(':')[0] ? pair.split(':')[0].trim() : pair.split(':')[0];
+        let value = pair.split(':')[1] ? pair.split(':')[1].trim() : pair.split(':')[1];
+        if (rule === 'background' || rule === 'opacity') {
+            continue;
+        }
+        const result = self.targetDom.body.querySelector(`h3#css${rule}`);
+        if (!result) {
+            self.invalidRules.push(pair);
+            continue;
+        }
+        console.log(self.invalidRules);
+        const valueValidationElement = result.nextSibling.nextSibling.nextSibling.nextSibling;
+        if (!(valueValidationElement instanceof HTMLTableElement)) {
+            continue;
+        }
+        // valueValidationElement.tBodies[0].rows[0].cells[0].innerHTML
+        let valuePresent = false;
+        for (const tableRow of valueValidationElement.tBodies[0].rows) {
+            if (tableRow.cells[0].innerHTML == value) {
+                valuePresent = true;
+                break;
+            }
+        }
+        if (!valuePresent) {
+            self.invalidRules.push(pair);
+        }
+    }
+}
+
+const runner = new Supported("http://localhost/adobe-plugin-ref.html", "http://localhost/style.css");
